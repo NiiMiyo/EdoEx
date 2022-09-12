6551cf2ae5f4493b1215d4d7d8710992d7550285
Douglas Sebastian <kagnanii.miyo@gmail.com>
2022-09-11 21:41:04 -0300

    pendulum artwork

--- cardimage/artwork.go
+++ cardimage/artwork.go
@@ -13,7 +13,8 @@ import (
 )
 
 var (
-	normalCardPosition = image.Rect(107, 210, 633, 736)
+	normalCardPosition   = image.Rect(107, 210, 633, 736)
+	pendulumCardPosition = image.Rect(68, 206, 670, 654)
 )
 
 func PutArtwork(img image.Image, card *models.Card) (image.Image, error) {
@@ -23,12 +24,19 @@ func PutArtwork(img image.Image, card *models.Card) (image.Image, error) {
 		return nil, err
 	}
 
-	size := normalCardPosition.Size()
+	var positionRect image.Rectangle
+	if card.HasSubType("pendulum") {
+		positionRect = pendulumCardPosition
+	} else {
+		positionRect = normalCardPosition
+	}
+
+	size := positionRect.Size()
 	width, height := uint(size.X), uint(size.Y)
 
 	artworkImage = resize.Resize(width, height, artworkImage, resize.Bilinear)
 	drawableArtwork := imagesutils.GetRGBA(img)
 
-	draw.Draw(drawableArtwork, normalCardPosition, artworkImage, image.Point{X: 0, Y: 0}, draw.Over)
+	draw.Draw(drawableArtwork, positionRect, artworkImage, image.Point{X: 0, Y: 0}, draw.Over)
 	return drawableArtwork, nil
 }
